5a3b04bab2783691566f0f09a3d414ab31efb9ca
fferro <francesco.ferro@yahooinc.com>
2024-07-17 10:58:41 +0100

    cleanup

--- config.go
+++ config.go
@@ -8,7 +8,7 @@
 // the host name to match on ("example.com"), and the second argument is the key
 // you want to retrieve ("Port"). The keywords are case insensitive.
 //
-//	port := ssh_config.Get("myhost", "Port")
+// 		port := ssh_config.Get("myhost", "Port")
 //
 // You can also manipulate an SSH config file and then print it or write it back
 // to disk.
@@ -310,7 +310,8 @@ func (u *UserSettings) doLoadConfigs() {
 			u.onceErr = err
 			return
 		}
-	})
+	},
+	)
 }
 
 func parseFile(filename string, ignoreMatchDirective bool) (*Config, error) {
